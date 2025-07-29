ac47128095d86bf3595e5aa610e6d051911f45b3
Aayush Sharma <135792547+aayushsh2003@users.noreply.github.com>
2025-07-29 21:04:30 +0530

    update monogo connect

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.use(cors({
 
 
 // DB + Server Start
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(5000, () => console.log("Server started on port 5000")))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(5000, () => console.log("Server running on 5000")))
+  .catch((err) => console.error(err));
